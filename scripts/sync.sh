dd4c6174401ac274cdd1131d1d3818c9a1cb5b53
Sarah Gibson <drsarahlgibson@gmail.com>
2025-11-25 20:00:54 +0000

    Update the sync script to use rclone

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    SOURCE_DIR="/tmp/books/"
-else
-    SOURCE_DIR="${TMPDIR}/books/"
-fi
+SOURCE_DIR="/tmp/books/"
+DEST_DIR="gdrive:devault/Atlas/Notes/Vaults/Library"
 
-DEST_DIR="${HOME}/Google\ Drive/My\ Drive/devault/Library"
+rclone sync --checksum "$SOURCE_DIR" "$DEST_DIR"
 
-rsync -rvc --delete "${SOURCE_DIR}" "${DEST_DIR}"
+rm -rf "$SOURCE_DIR"
