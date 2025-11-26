00a06f417032ed73248f70d66a6df69904dc0297
Sarah Gibson <drsarahlgibson@gmail.com>
2025-11-26 11:46:18 +0000

    sync.sh outputs to log file

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,6 +3,6 @@
 SOURCE_DIR="/tmp/books/"
 DEST_DIR="gdrive:devault/Atlas/Notes/Vaults/Library"
 
-rclone sync --checksum "$SOURCE_DIR" "$DEST_DIR"
+rclone sync -P --checksum --log-file /var/log/rclone.txt "$SOURCE_DIR" "$DEST_DIR"
 
 rm -rf "$SOURCE_DIR"
