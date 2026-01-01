98023dabc29bf1b531217eea8fe26b6db52668f7
Sarah Gibson <drsarahlgibson@gmail.com>
2026-01-01 13:37:17 +0000

    Update destination dir in sync.sh script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_DIR="/tmp/books/"
-DEST_DIR="gdrive:devault/Atlas/Notes/Vaults/Library"
+DEST_DIR="gdrive:Atlas/Notes/Vaults/Library"
 
 rclone sync -P --checksum --log-file /var/log/rclone.txt "$SOURCE_DIR" "$DEST_DIR"
 
