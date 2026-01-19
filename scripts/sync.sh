d228ad3bd245a3eb84fb0bef6e45ffed2ccce6fa
Sarah Gibson <drsarahlgibson@gmail.com>
2026-01-19 08:32:58 +0000

    Ensure sync script pulls repo first

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -4,6 +4,7 @@ cd "${HOME}/Documents/github/goodreads2md" || exit
 .venv/bin/python scripts/goodreads.py "${HOME}/Documents/github/devault/Atlas/Notes/Vaults/Library"
 
 cd "${HOME}/Documents/github/devault" || exit
+git pull
 git add .
 git commit -m "$(date '+%Y-%m-%dT%H:%M') Update Library files"
 git push
