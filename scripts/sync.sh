3a85c3b9490e0609389e066b18245d7c7959bf7c
Sarah Gibson <drsarahlgibson@gmail.com>
2026-01-20 16:19:51 +0000

    Restructure sync script to pull destination repo before making changes

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd "${HOME}/Documents/github/goodreads2md" || exit
-.venv/bin/python scripts/goodreads.py "${HOME}/Documents/github/devault/Atlas/Notes/Vaults/Library"
-
 cd "${HOME}/Documents/github/devault" || exit
 git pull
+
+"${HOME}/Documents/github/goodreads2md/.venv/bin/python" scripts/goodreads.py "${HOME}/Documents/github/devault/Atlas/Notes/Vaults/Library"
+
 git add .
 git commit -m "$(date '+%Y-%m-%dT%H:%M') Update Library files"
 git push
