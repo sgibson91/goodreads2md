4faf06e446dcf5f0904953d43eeb461d8fe4df64
Sarah Gibson <drsarahlgibson@gmail.com>
2026-01-20 16:22:26 +0000

    Add comments and set explicit path to sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,7 +3,12 @@
 cd "${HOME}/Documents/github/devault" || exit
 git pull
 
-"${HOME}/Documents/github/goodreads2md/.venv/bin/python" scripts/goodreads.py "${HOME}/Documents/github/devault/Atlas/Notes/Vaults/Library"
+# Python executable
+"${HOME}/Documents/github/goodreads2md/.venv/bin/python" \
+    # Path to script
+    "${HOME}/Documents/github/goodreads2md/scripts/goodreads.py" \
+    # Path to Obsidian Library
+    "${HOME}/Documents/github/devault/Atlas/Notes/Vaults/Library"
 
 git add .
 git commit -m "$(date '+%Y-%m-%dT%H:%M') Update Library files"
