6b7e8a71fad68e148f9e91274f3f4c3776b14d30
Sarah Gibson <drsarahlgibson@gmail.com>
2026-01-20 16:30:24 +0000

    Refactor sync script again

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -3,13 +3,10 @@
 cd "${HOME}/Documents/github/devault" || exit
 git pull
 
-# Python executable
-"${HOME}/Documents/github/goodreads2md/.venv/bin/python" \
-    # Path to script
-    "${HOME}/Documents/github/goodreads2md/scripts/goodreads.py" \
-    # Path to Obsidian Library
-    "${HOME}/Documents/github/devault/Atlas/Notes/Vaults/Library"
+cd "${HOME}/Documents/github/goodreads2md" || exit
+.venv/bin/python scripts/goodreads.py "${HOME}/Documents/github/devault/Atlas/Notes/Vaults/Library"
 
+cd "${HOME}/Documents/github/devault" || exit
 git add .
 git commit -m "$(date '+%Y-%m-%dT%H:%M') Update Library files"
 git push
